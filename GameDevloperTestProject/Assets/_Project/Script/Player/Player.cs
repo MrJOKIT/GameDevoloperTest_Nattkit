c784c55278df921ce79b8df51e759db076838746
Nattakit Namwong <nuttakit.namw@bumail.net>
2025-05-09 21:08:00 +0700

    Prepare Use and Equip item

--- GameDevloperTestProject/Assets/_Project/Script/Player/Player.cs
+++ GameDevloperTestProject/Assets/_Project/Script/Player/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Stat")] 
+    [SerializeField] private int playerMaxHealth = 5;
+    [SerializeField] private int playerHealth = 5;
+    [SerializeField] private int playerDamage = 1;
     [Header("Interact")]
     [SerializeField] private bool canInteract = false;
     public bool CanInteract { get { return canInteract; } set { canInteract = value; } }
